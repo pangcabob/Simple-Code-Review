14f574e2c126c344ecae93334a4a45fed35b8b3f
pangcabob <35266739+pangcabob@users.noreply.github.com>
2018-01-09 09:45:54 -0700

    Ignore
    I made a change

---
+++ CSharp Code Review/Code Review/Salutation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Review
+{
+    class Salutation
+    {
+        private string _Greeting;
+        private string _Farewell;
+        public Salutation(string greetingMessage, String farewellMessage)
+        {
+            _Greeting = greetingMessage;
+            _Farewell = farewellMessage;
+        }
+
+        public string Greet()
+        {
+            return _Greeting;
+        }
+        public string SayFarewell()
+        {
+            return _Farewell;
+        }
+    }
+}
