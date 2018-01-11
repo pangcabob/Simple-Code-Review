99607f3bb0cf3202dd9041e18cf752b65e8abffc
pangcabob <35266739+pangcabob@users.noreply.github.com>
2018-01-11 16:25:52 -0700

    aaa

---
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReview
+{
+    public class Salutation
+
+    {
+        /// <summary>
+        /// The Salutation class represents something that can say hello or goodbye
+        /// </summary>
+        #region Properties and Fields -What the Salutation objects will "Look like"
+        //Fields
+        private string _Greeting;
+        private string _Farewell;
+        #endregion
+
+        #region Constructor and Methods- How the Salutation objects will "believe"
+        /// <summary>
+        /// Instantiates a Salutation object with appropriate messages
+        /// </summary>
+        /// <param name="greetingMessage">The message to use when greeting</param>
+        /// <param name="farewellMessage">The message to use to say goodbye</param>
+        public Salutation(string greetingMessage, string farewellMessage)
+        {
+            _Greeting = greetingMessage;
+            _Farewell = farewellMessage;
+        }
+       
+
+        public string Greet()
+        {
+            return _Greeting;
+        }
+
+        public string SayFarewell()
+        {
+            return _Farewell;
+        }
+        #endregion
+    }
+}
